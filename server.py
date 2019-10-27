106a33480bdc1f45f691049590032f6167fc62ed
For Ka Lau <c1941440@comsc-102722.dsa.cf.ac.uk>
2019-10-27 22:52:08 +0000

    my portfolio website

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/<string:page_name>')
+def html_pages(page_name):
+	return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	return 'form submitted!'
