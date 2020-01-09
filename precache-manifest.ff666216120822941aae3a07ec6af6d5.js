4084f327df183cb3faa0da9388a29ec0304bbd91
Derrick Lau <derrickalau@gmail.com>
2020-01-09 15:01:23 +0000

    Updates

---
+++ precache-manifest.ff666216120822941aae3a07ec6af6d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ecbcb2c556bf8d1c1714606657b867",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "54459628a71df76a9030",
+    "url": "/portfolio/static/css/main.929fcc5d.chunk.css"
+  },
+  {
+    "revision": "caf00e6fbf23993908c5",
+    "url": "/portfolio/static/js/2.ad6e89a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.ad6e89a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54459628a71df76a9030",
+    "url": "/portfolio/static/js/main.efd0ad78.chunk.js"
+  },
+  {
+    "revision": "a7ea166ea125105aa0fd",
+    "url": "/portfolio/static/js/runtime-main.e697b906.js"
+  }
+]);
