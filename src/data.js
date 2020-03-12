6b5c5f6dec336ab946fcc0df23cbf46a9656f805
Derrick Lau <derrickalau@gmail.com>
2020-03-12 22:10:55 +0000

    updated data

--- src/data.js
+++ src/data.js
@@ -3,10 +3,10 @@ export default
         name :'',
         headerTagline: [
                         'Hi 👋 You found me! I am Derrick Lau.',
-                        'Full-stack Web developer',          
+                        'Software Developer',          
         ],
         contactEmail:'lauf@cardiff.ac.com',
-        abouttext: "Hi there I am a MSc student studying Computing. I started developing web apps in 2018, with experience in HTML, CSS, Javascript all the way to modern techs like React and C# asp.net core.",
+        abouttext: "Hi there I am a MSc student studying Computing. I started developing web apps in 2018, with experience in HTML, CSS, Javascript all the way to modern technologies like React and Java Spring boot.",
         aboutImage:'https://live.staticflickr.com/2279/2020187066_f3bca7cbac_b.jpg',
         ShowAboutImage:true,
   
@@ -14,32 +14,25 @@ export default
            {
             id: 1,
             title:'Face-detection-app', 
-             service:'Web Development (React, Express)', 
+             service:'Web Development (Reactjs, Express.js)', 
 
              imageSrc:"https://live.staticflickr.com/65535/49356444676_e4b81ad664_h.jpg",
 
              url:'https://github.com/Derrick-lau/Face-detection-app/'
             },
-            {
-                id: 2,
-                title: 'Library Management System',
-                service: 'Web Development (React, Express)',
-                imageSrc: "https://live.staticflickr.com/65535/49355991563_3e7d54c7ac_h.jpg",
-                url: 'https://github.com/Derrick-lau/LMS/'
-            },
             { 
-                id: 3,
+                id: 2,
                 title: 'Migrating Library Management System to Java (developing......)',
                 service: 'Web Development (React-typescript, Redux, Java Spring boot)',
                 imageSrc: "https://live.staticflickr.com/65535/49355991563_3e7d54c7ac_h.jpg",
-                url: 'https://github.com/Derrick-lau/LMS-JAVA/'
+                url: 'https://github.com/Derrick-lau/library-management-java'
             },
             { 
-                id: 4,
-                title: 'Migrating Face-detection-app to ASP.NET CORE (developing......)',
-                service: 'Web Development (React-typescript, C# ASP.NET CORE)',
+                id: 3,
+                title: 'E-commerce web app  (developing......)',
+                service: 'Web Development (Nextjs-typescript(SSR), Redux, Python flask)',
                 imageSrc: "https://live.staticflickr.com/65535/49356652602_0cab5ac61c_c.jpg",
-                url: 'https://github.com/Derrick-lau/Face-detection-network'
+                url: 'https://github.com/Derrick-lau/e-commerce-app'
             },
         ],
         social: [
